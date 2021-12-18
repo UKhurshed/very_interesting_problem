159c8f8c408f4fba24f26b212ba22ec92fb8b052
Ukhurshed <khurshedumarov777@gmail.com>
2021-12-18 10:23:27 +0500

    init commit

---
+++ very_interesting_problem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  very_interesting_problem
+//
+//  Created by Khurshed Umarov on 18.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
